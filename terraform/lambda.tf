68e8634ae9b9f344bb017fbffe917e54b0118024
Jake Reck <jakereck@gmail.com>
2025-05-15 10:00:36 -0500

    Updating lambda functions to have the permissions for dynamodb

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -13,6 +13,32 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_policy" "dynamodb_access" {
+  name = "lambda-dynamodb-access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query"
+        ],
+        Resource = aws_dynamodb_table.todos.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_dynamodb_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.dynamodb_access.arn
+}
+
 resource "aws_iam_policy_attachment" "lambda_policy" {
   name       = "lambda_policy"
   roles      = [aws_iam_role.lambda_exec.name]
@@ -25,6 +51,8 @@ resource "aws_lambda_function" "create_todo" {
   role          = aws_iam_role.lambda_exec.arn
   handler       = "main.handler"
   filename      = "${path.module}/../lambdas/createTodo.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambdas/createTodo.zip")
+
 
   environment {
     variables = {
@@ -39,6 +67,8 @@ resource "aws_lambda_function" "get_todos" {
   role          = aws_iam_role.lambda_exec.arn
   handler       = "main.handler"
   filename      = "${path.module}/../lambdas/getTodos.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambdas/getTodos.zip")
+
 
   environment {
     variables = {
@@ -53,6 +83,8 @@ resource "aws_lambda_function" "update_todo" {
   role          = aws_iam_role.lambda_exec.arn
   handler       = "main.handler"
   filename      = "${path.module}/../lambdas/updateTodo.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambdas/updateTodo.zip")
+
 
   environment {
     variables = {
@@ -67,6 +99,8 @@ resource "aws_lambda_function" "delete_todo" {
   role          = aws_iam_role.lambda_exec.arn
   handler       = "main.handler"
   filename      = "${path.module}/../lambdas/deleteTodo.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambdas/deleteTodo.zip")
+
 
   environment {
     variables = {
